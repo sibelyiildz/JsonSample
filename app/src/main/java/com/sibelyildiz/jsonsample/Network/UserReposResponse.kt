c9d183da3ba3cbd161a537adaf9efae8c5a7469b
Sibel Yıldız <sibelyldz2012@gmail.com>
2020-08-12 18:44:42 +0300

    initial commit

---
+++ app/src/main/java/com/sibelyildiz/jsonsample/Network/UserReposResponse.kt
@@ -0,0 +1,16 @@
+package com.sibelyildiz.jsonsample.Network
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║  Created by Sibel YILDIZ   ║
+╠════════════════════════════╣
+║ sibelyldz2012@gmail.com    ║
+╠════════════════════════════╣
+║     12/08/2020 - 18:36     ║
+╚════════════════════════════╝
+ */
+
+data class UserReposResponse(
+    val data: List<String>
+)
---
+++ app/src/main/java/com/sibelyildiz/jsonsample/data/RepoModel.kt
@@ -0,0 +1,12 @@
+package com.sibelyildiz.jsonsample.data
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║  Created by Sibel YILDIZ   ║
+╠════════════════════════════╣
+║ sibelyldz2012@gmail.com    ║
+╠════════════════════════════╣
+║     12/08/2020 - 18:43     ║
+╚════════════════════════════╝
+ */
